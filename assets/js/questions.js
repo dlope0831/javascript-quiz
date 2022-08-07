6a60761e8293f5d8188865192ebd979e768c317c
Daniela Lopez <dlope0831@gmail.com>
2022-08-07 11:30:55 -0500

    added questions to questions.js

---
+++ assets/js/questions.js
@@ -0,0 +1,41 @@
+const myQuestions =[
+    {
+        "question": "What are all types of Pop up boxes available in JavaScript?",
+        "answers": [
+            "Alert"
+            ,"Prompt"
+            ,"Confirm"
+            ,"All of the above"
+        ],
+        "correct":3
+    };
+    {
+        "question": "What is not a JavaScript Data Types?",
+        "answers": [
+            "Number"
+            ,"String"
+            ,"Type"
+            ,"Boolean"
+        ],
+        "correct":2
+    };
+    {
+        "question": "What are all the looping structures in JavaScript?",
+        "answers": [
+            "For"
+            ,"While"
+            ,"Do-While Loops"
+            ,"All of the above"
+        ],
+        "correct":3
+    };
+    {
+        "question": "What are the scopes of a variable in JavaScript?",
+        "answers": [
+            "International Variables"
+            ,"Global Variables"
+            ,"Local Varaiables"
+        ],
+        "correct":0
+    }
+];
---
+++ script.js
@@ -0,0 +1,41 @@
+var timerEl = document.getElementById('timer');
+var startQuizBtn = document.getElementById ('start-quiz');
+
+var playersScore = [];
+var correctAnswers =["a","b","c","d"];
+// var myQuestions =[""];
+
+function startQuiz() {
+    playersScore = [];
+    
+    }
+function allQuestions() {
+
+}
+
+function allAnswers () {
+
+}
+startQuizBtn.addEventListener("click", startQuiz);
+
+
+function countdown(event) {
+    var timeLeft = 75;
+    // event.preventDefault();
+  var timeInterval = setInterval(function () {
+    if (timeLeft > 1) {
+      timerEl.textContent = timeLeft + ' seconds remaining';
+      timeLeft--;
+    }
+    else if (timeLeft === 1) {
+      timerEl.textContent = timeLeft + ' second remaining';
+      timeLeft--;
+    } 
+    else {
+      timerEl.textContent = '';
+      clearInterval(timeInterval);
+      displayMessage();
+    }
+  }, 1000);
+}
+countdown();
